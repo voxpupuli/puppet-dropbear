efc6806bae63eb46efb1ea1c39b702207a9c0831
Kyle Anderson <kyle@xkyle.com>
2013-10-20 11:57:39 -0700

    Added redhat support, and adjusted paramater names to be more generic

--- manifests/params.pp
+++ manifests/params.pp
@@ -16,6 +16,16 @@ class dropbear::params {
       $service_name   = 'dropbear'
       $rsakey         = '/etc/dropbear/dropbear_rsa_host_key'
       $dsskey         = '/etc/dropbear/dropbear_dss_host_key'
+      $cfg_file       = '/etc/default/dropbear'
+      $cfg_template   = 'dropbear/debian.erb'
+    }
+    'RedHat': {
+      $package_name   = 'dropbear'
+      $service_name   = 'dropbear'
+      $rsakey         = '/etc/dropbear/dropbear_rsa_host_key'
+      $dsskey         = '/etc/dropbear/dropbear_dss_host_key'
+      $cfg_file       = '/etc/sysconfig/dropbear'
+      $cfg_template   = 'dropbear/redhat.erb'
     }
     default: {
       fail("Unsupported osfamily: ${::osfamily}")
--- spec/classes/dropbear_spec.rb
+++ spec/classes/dropbear_spec.rb
@@ -11,17 +11,49 @@ describe 'dropbear', :type => :class do
     end
   end
 
-  context "On Debian" do
+  describe "On Debian" do
     let(:facts) {{ :osfamily => 'Debian' }}
     it { should include_class("dropbear::params") }
     it { should contain_package('dropbear') }
+    it { should contain_service('dropbear') }
+    it { should contain_file('/etc/default/dropbear/').with(
+        :owner => 'root',
+        :group => 'root',
+        :mode  => '0444'
+      )
+    }
+    context "When an alternate port is given" do
+      let(:params) {{ :port => '42' }}
+      it { should contain_file('/etc/default/dropbear/').with_content(/DROPBEAR_PORT=42/) }
+    end
+    context "When a banner is specified" do
+      let(:params) {{ :banner => '/etc/test_banner' }}
+      it { should contain_file('/etc/default/dropbear/').with_content(/DROPBEAR_BANNER="\/etc\/test_banner"/) }
+    end
   end
 
-  #TODO
-  #context "On RedHat" do
-  #  let(:facts) {{ :osfamily => 'RedHat' }}
-  #  it { should include_class("dropbear::params") }
-  #  it { should contain_package('dropbear') }
-  #end
+  describe "On RedHat" do
+    let(:facts) {{ :osfamily => 'RedHat' }}
+    it { should include_class("dropbear::params") }
+    it { should contain_package('dropbear') }
+    it { should contain_service('dropbear') }
+    it { should contain_file('/etc/sysconfig/dropbear/').with(
+        :owner => 'root',
+        :group => 'root',
+        :mode  => '0444'
+      )
+    }
+    context "When an alternate port is given" do
+      let(:params) {{ :port => '42' }}
+      it { should contain_file('/etc/sysconfig/dropbear/').with_content(/-p 42/) }
+    end
+    context "When a banner is specified" do
+      let(:params) {{ :banner => '/etc/test_banner' }}
+      it { should contain_file('/etc/sysconfig/dropbear/').with_content(/ -b \/etc\/test_banner /) }
+    end
+    context "When a banner is NOT specified" do
+      it { should_not contain_file('/etc/sysconfig/dropbear/').with_content(/ -b /) }
+    end
+  end
 end
 
