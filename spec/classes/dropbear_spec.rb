318a097f748ddf7c67b9276d740bd385605092cf
Sebastien Badia <seb@sebian.fr>
2014-11-05 00:39:46 +0100

    (spec) QA - Unpin rspec and lint versions
    Unpin rspec version, therefore update the tests (remove include_class, and fix should).
    
    Unpin puppet-lint gem.

--- spec/classes/dropbear_spec.rb
+++ spec/classes/dropbear_spec.rb
@@ -4,7 +4,7 @@ describe 'dropbear', :type => :class do
 
   describe 'On an unknown operating system' do
     let(:facts) {{ :osfamily => 'Unknown' }}
-    it "should fail" do
+    it "is_expected.to fail" do
       expect do
         subject
       end.to raise_error(Puppet::Error, /Unsupported osfamily/)
@@ -13,10 +13,10 @@ describe 'dropbear', :type => :class do
 
   describe "On Debian" do
     let(:facts) {{ :osfamily => 'Debian' }}
-    it { should include_class("dropbear::params") }
-    it { should contain_package('dropbear') }
-    it { should contain_service('dropbear') }
-    it { should contain_file('/etc/default/dropbear/').with(
+    it { is_expected.to contain_class("dropbear::params") }
+    it { is_expected.to contain_package('dropbear') }
+    it { is_expected.to contain_service('dropbear') }
+    it { is_expected.to contain_file('/etc/default/dropbear/').with(
         :owner => 'root',
         :group => 'root',
         :mode  => '0444'
@@ -24,20 +24,20 @@ describe 'dropbear', :type => :class do
     }
     context "When an alternate port is given" do
       let(:params) {{ :port => '42' }}
-      it { should contain_file('/etc/default/dropbear/').with_content(/DROPBEAR_PORT=42/) }
+      it { is_expected.to contain_file('/etc/default/dropbear/').with_content(/DROPBEAR_PORT=42/) }
     end
     context "When a banner is specified" do
       let(:params) {{ :banner => '/etc/test_banner' }}
-      it { should contain_file('/etc/default/dropbear/').with_content(/DROPBEAR_BANNER="\/etc\/test_banner"/) }
+      it { is_expected.to contain_file('/etc/default/dropbear/').with_content(/DROPBEAR_BANNER="\/etc\/test_banner"/) }
     end
   end
 
   describe "On RedHat" do
     let(:facts) {{ :osfamily => 'RedHat' }}
-    it { should include_class("dropbear::params") }
-    it { should contain_package('dropbear') }
-    it { should contain_service('dropbear') }
-    it { should contain_file('/etc/sysconfig/dropbear/').with(
+    it { is_expected.to contain_class("dropbear::params") }
+    it { is_expected.to contain_package('dropbear') }
+    it { is_expected.to contain_service('dropbear') }
+    it { is_expected.to contain_file('/etc/sysconfig/dropbear/').with(
         :owner => 'root',
         :group => 'root',
         :mode  => '0444'
@@ -45,14 +45,14 @@ describe 'dropbear', :type => :class do
     }
     context "When an alternate port is given" do
       let(:params) {{ :port => '42' }}
-      it { should contain_file('/etc/sysconfig/dropbear/').with_content(/-p 42/) }
+      it { is_expected.to contain_file('/etc/sysconfig/dropbear/').with_content(/-p 42/) }
     end
     context "When a banner is specified" do
       let(:params) {{ :banner => '/etc/test_banner' }}
-      it { should contain_file('/etc/sysconfig/dropbear/').with_content(/ -b \/etc\/test_banner /) }
+      it { is_expected.to contain_file('/etc/sysconfig/dropbear/').with_content(/ -b \/etc\/test_banner /) }
     end
     context "When a banner is NOT specified" do
-      it { should_not contain_file('/etc/sysconfig/dropbear/').with_content(/ -b /) }
+      it { is_expected.to_not contain_file('/etc/sysconfig/dropbear/').with_content(/ -b /) }
     end
   end
 end
