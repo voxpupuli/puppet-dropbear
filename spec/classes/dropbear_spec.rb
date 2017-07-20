fc3c14802a3f1e6d5564b5aae0f8d59fed5382a0
Sebastien Badia <seb@sebian.fr>
2017-07-20 16:11:58 +0200

    spec: Fix syntax and metadata file

--- spec/classes/dropbear_spec.rb
+++ spec/classes/dropbear_spec.rb
@@ -7,38 +7,42 @@ describe 'dropbear' do
         facts
       end
 
-      conf_file = facts[:osfamily] == 'RedHat' ? "/etc/sysconfig/dropbear" : "/etc/default/dropbear"
+      conf_file = facts[:osfamily] == 'RedHat' ? '/etc/sysconfig/dropbear' : '/etc/default/dropbear'
 
       context 'with all defaults' do
         it { is_expected.to compile.with_all_deps }
         it { is_expected.to contain_class('dropbear') }
-        it { is_expected.to contain_class("dropbear::params") }
+        it { is_expected.to contain_class('dropbear::params') }
         it { is_expected.to contain_package('dropbear') }
         it { is_expected.to contain_service('dropbear') }
-        it { is_expected.to contain_file(conf_file).with(
-          'owner' => 'root',
-          'group' => 'root',
-          'mode'  => '0444'
-        )}
+        it {
+          is_expected.to contain_file(conf_file).with(
+            'owner' => 'root',
+            'group' => 'root',
+            'mode'  => '0444'
+          )
+        }
       end
 
-      context "When an alternate port is given" do
-        let(:params) {{ :port => '42' }}
+      context 'When an alternate port is given' do
+        let(:params) { { port: '42' } }
+
         it { is_expected.to contain_file(conf_file).with_content(%r{-p 42}) }
       end
-      context "When a banner is specified" do
-        let(:params) {{ :banner => '/etc/test_banner' }}
+      context 'When a banner is specified' do
+        let(:params) { { banner: '/etc/test_banner' } }
+
         it { is_expected.to contain_file(conf_file).with_content(%r{-b /etc/test_banner}) }
       end
-      context "When a banner is NOT specified" do
-        it { is_expected.to_not contain_file(conf_file).with_content(%r{-b}) }
+      context 'When a banner is NOT specified' do
+        it { is_expected.not_to contain_file(conf_file).with_content(%r{-b}) }
       end
-
     end
   end
 
   describe 'On an unknown operating system' do
-    let(:facts) {{ :osfamily => 'Unknown' }}
-    it { expect { catalogue }.to raise_error(Puppet::Error, /Unsupported osfamily/) }
+    let(:facts) { { osfamily: 'Unknown' } }
+
+    it { expect { catalogue }.to raise_error(Puppet::Error, %r{Unsupported osfamily}) }
   end
 end
